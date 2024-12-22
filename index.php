60fb90ddc66286d4c53b9d0532023505f75491ff
Ilham Aditya <ilhamadityapb@gmail.com>
2024-12-22 08:59:16 +0700

    index beranda

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="title" content="File Index">
+    <title>File index</title>
+</head>
+<body>
+    <h1>File undex</h1>
+</body>
+</html>
